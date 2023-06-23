aa3f8263f3ca9b68cc98e5542dbd879f78572651
fry <E0968906@u.nus.edu>
2023-06-23 14:33:47 +0800

    Disable bable plugins

--- track-me/babel.config.js
+++ track-me/babel.config.js
@@ -1,3 +1,4 @@
+/*
 module.exports = function(api) {
   api.cache(true);
   return {
@@ -5,3 +6,4 @@ module.exports = function(api) {
     plugins: [require.resolve("expo-router/babel"),"module:react-native-dotenv"],
   };
 };
+*/
