620d76d4b59a41867215935314752199530e50a7
fry <E0968906@u.nus.edu>
2023-06-23 12:07:37 +0800

    Update eas.json

--- track-me/App.js
+++ track-me/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+/*
 export default function App() {
   return (
     <View style={styles.container}>
@@ -18,3 +19,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
